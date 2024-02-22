16cfa796ce034d5988eb35af54b4cfe4763dcd87
darkstarlegend <anshulsinghkamboj@gmail.com>
2024-02-22 22:34:18 +0530

    t1

---
+++ template.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+import logging
+
+#logging string
+
+logging.basicConfig(Level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+projetc_name= 'cnnclassifier'
